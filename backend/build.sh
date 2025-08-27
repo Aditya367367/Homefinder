ed025c8cbc685d98904d0aeed540ea7093da28f0
Aditya Chauhan <suryachauhan367367@gamil.com>
2025-08-27 16:19:15 +0530

    added

---
+++ backend/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
+  echo "Superuser creation command finished."
+fi
+
+# Collect static files
+python manage.py collectstatic --noinput
